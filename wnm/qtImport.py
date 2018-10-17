13d93d3d96ce23e3843a20ec1ffd92b213dc6d9f
Basile MARCHAND <basile.marchand@mines-paristech.fr>
2018-10-17 22:09:02 +0200

    Bug Fix --- add a try block for the QtWebView import since its name change since qt5.5

--- wnm/qtImport.py
+++ wnm/qtImport.py
@@ -1,8 +1,13 @@
 
 from PyQt5.QtCore import Qt, QUrl
 import PyQt5.QtCore as QtCore
-from PyQt5.QtWebKitWidgets import QWebView
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QListWidget, QComboBox, QGridLayout, QGroupBox, QVBoxLayout, QLineEdit, QCheckBox, QPushButton, QTreeWidget, QTreeWidgetItem, QTreeWidgetItemIterator, QFileDialog
 from PyQt5.QtPrintSupport import QPrinter
 import PyQt5.QtGui
 import PyQt5.QtWidgets
+
+
+try:
+    from PyQt5.QtWebKitWidgets import QWebView
+except ImportError:
+    from PyQt5.QtWebEngineWidgets import QWebEngineView as QWebView
